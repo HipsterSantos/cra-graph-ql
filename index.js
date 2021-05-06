39dbc0b1b7249312d56ddf0080426005af0f03a2
HipsterSantos <santoscampos269@gmail.com>
2021-05-06 04:16:00 +0100

    added some packeges and app structure updated

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+const express = require('express'); 
+const graphqlHTTP = require('express-graphql');
 const { graphql, buildSchema } = require('graphql'); 
+const app = express();
+
 
 const schema = buildSchema(`   
     type Query{
-        message: String 
+      users: [User!]! 
+    }
+
+    type User{
+        id: ID!
+        email: String!
+        name: String!
+        avatarUrl: String!
     }
 `)
 
+const db  ={ users:[
+        {id: '1', email: 'santos@d.co',name:"Santos"},
+        {id:'12',email:"sjeiss@gma.com",name:"alex"}
+    ]}
 const rootValue = { 
-    message: ()=> 'Graphql works and this is my resolver'
+        users:() => [
+        {id: '1', email: 'santos@d.co',name:"Santos"},
+        {id:'12',email:"sjeiss@gma.com",name:"alex"}
+    ]
 }
 
 graphql(
-    schema, 
+        schema, 
     `{
-        message
+        users{
+           name
+        }
     }`,
     rootValue
-).then(console.log).catch(console.log)
+).then(console.log).catch(console.error)
+
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql:true
+}))
+
+app.listen(3000,console.log);
